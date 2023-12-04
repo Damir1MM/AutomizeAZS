ec8b84f23e066d7088538813b773a0f40674dd72
Damir1MM <damir.m.m.123@gmail.com>
2023-12-04 16:34:31 +0400

    Тело метода FreePos

--- automizeAZS/automizeAZS/Program.cs
+++ automizeAZS/automizeAZS/Program.cs
@@ -2,7 +2,35 @@
 
 class FreePos
 {
+    bool[][] parkingSpaces;
 
+    public FreePos()
+    {
+
+        parkingSpaces = new bool[][]
+        {
+            new bool[2],
+            new bool[2],
+            new bool[2],
+            new bool[2],
+            new bool[4]
+        };
+    }
+
+    public void CarArrived(int columnNumber, int spaceNumber)
+    {
+        if (columnNumber >= 1 && columnNumber <= parkingSpaces.Length)
+        {
+            if (spaceNumber >= 1 && spaceNumber <= parkingSpaces[columnNumber - 1].Length)
+            {
+                if (!parkingSpaces[columnNumber - 1][spaceNumber - 1])
+                {
+                    parkingSpaces[columnNumber - 1][spaceNumber - 1] = true;
+                    Console.WriteLine($"Место {spaceNumber} на колонке {columnNumber} теперь занято.");
+                }
+            }
+        }
+    }
 }
 
 class Program
